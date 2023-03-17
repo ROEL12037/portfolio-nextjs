645b015449410f9dde20ab2eb648c7c16574d8df
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-03-17 13:21:59 -0500

    modify crossorigin to crossOrigin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,7 +16,7 @@ export default function Document() {
 				<link
 					rel="preconnect"
 					href="https://fonts.gstatic.com"
-					crossorigin="anonymous"
+					crossOrigin="anonymous"
 				/>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
