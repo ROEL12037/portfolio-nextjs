f4a3e232ba5444bcee231b187834aa0d432b54ab
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-05-01 16:55:28 -0500

    import useRouter to conditionally render styles on navbar links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,37 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+	const router = useRouter();
+
+	// styles
+	const activeStyles = "my-auto px-1 py-1 bg-cyan-800";
+	const inactiveStyles = "my-auto px-1 py-1 hover:text-cyan-800";
+
 	return (
-		<header className="">
+		<header className="h-8 border-b-2">
 			<nav className="flex flex-row justify-between">
 				<ul className="flex">
-					<li className="my-2 px-1 py-1 bg-cyan-800">
+					<li
+						className={
+							router.pathname == "/" ? `${activeStyles}` : `${inactiveStyles}`
+						}>
 						<Link href={"/"}>Roel Codes</Link>
 					</li>
-					<li className="mx-1 my-auto hover:text-cyan-800">
+					<li
+						className={
+							router.pathname == "/projects"
+								? `${activeStyles}`
+								: `${inactiveStyles}`
+						}>
 						<Link href={"/projects"}>Projects</Link>
 					</li>
-					<li className="mx-1 my-auto hover:text-cyan-800">
+					<li
+						className={
+							router.pathname == "/contact"
+								? `${activeStyles}`
+								: `${inactiveStyles}`
+						}>
 						<Link href={"/contact"}>Contact</Link>
 					</li>
 				</ul>
