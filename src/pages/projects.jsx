c19fbce423fdde3c2861703e8e84caebd901c506
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-05-03 21:25:25 -0500

    ensure project cards are the same size

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-
 import useSWR from "swr";
 import Spinner from "@/components/Spinner";
-
 import { formatDate } from "@/utils/dateHelpers";
 
 const fetcher = async (url) => {
@@ -23,66 +21,63 @@ export default function Projects({ starredRepos }) {
 
 	return (
 		<main className="flex items-center justify-center w-full min-h-[92vh]">
-			<section className="flex flex-col items-center">
+			<section className="flex flex-col items-center mx-4 mb-4">
 				<h2 className="mb-4 text-4xl text-cerulean">Recent Projects</h2>
 
-				<section className="flex flex-wrap w-full sm:flex-row flex-col">
+				<section className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
 					{data.map((repo) => {
 						return (
 							<section
-								className="w-full sm:w-1/2 h-1/2 p-2"
+								className="bg-white rounded-lg shadow-lg"
 								key={repo.id}>
-								<div className="bg-white rounded-lg shadow-lg min-h-[150px]">
-									<div className="px-6 py-4">
-										<h3 className="font-bold text-gray-800 text-xl mb-2">
+								<div className="px-6 py-4">
+									<h3 className="font-bold text-gray-800 text-xl mb-2">
+										{repo.repo.name}
+									</h3>
+									<Image
+										src="https://via.placeholder.com/440x248"
+										alt="card image"
+										width={440}
+										height={248}
+										className=""
+									/>
+									<p className="text-gray-700 text-base">
+										Demo:{" "}
+										<a
+											href={repo.repo.homepage}
+											target="_blank"
+											className="hover:text-cerulean underline font-semibold">
 											{repo.repo.name}
-										</h3>
-										<Image
-											src="https://via.placeholder.com/640x360"
-											alt="Card image"
-											width={640}
-											height={360}
-										/>
-										<p className="text-gray-700 text-base">
-											Demo:{" "}
-											<a
-												href={repo.repo.homepage}
-												target="_blank"
-												className="hover:text-cerulean underline font-semibold">
-												{repo.repo.name}
-											</a>
-										</p>
-										<p className="text-gray-700 text-base">
-											Created: {formatDate(repo.repo.created_at)}
-										</p>
-									</div>
+										</a>
+									</p>
+									<p className="text-gray-700 text-base">
+										Created: {formatDate(repo.repo.created_at)}
+									</p>
 								</div>
 							</section>
 						);
 					})}
-					<section className="w-full sm:w-1/2 h-1/2 p-2">
-						<div className="bg-white rounded-lg shadow-lg min-h-[150px]">
-							<div className="px-6 py-4">
-								<h3 className="font-bold text-gray-800 text-xl mb-2">
-									Want to see more?
-								</h3>
-								<Image
-									src="https://via.placeholder.com/640x360"
-									alt="Card image"
-									width={640}
-									height={360}
-								/>
-								<p className="text-gray-700 text-base">
-									Check out the rest of my public repositories{" "}
-									<a
-										href="https://github.com/ROEL12037?tab=repositories"
-										className="hover:text-cerulean underline font-semibold"
-										target="_blank">
-										here
-									</a>
-									.
-								</p>
-							</div>
+					<section className="bg-white rounded-lg shadow-lg">
+						<div className="px-6 py-4">
+							<h3 className="font-bold text-gray-800 text-xl mb-2">
+								Want to see more?
+							</h3>
+							<Image
+								src="https://via.placeholder.com/448x248"
+								alt="card image"
+								width={440}
+								height={248}
+							/>
+							<p className="text-gray-700 text-base">
+								Check out the rest of my public repositories{" "}
+								<a
+									href="https://github.com/ROEL12037?tab=repositories"
+									className="hover:text-cerulean underline font-semibold"
+									target="_blank">
+									here
+								</a>
+								.
+							</p>
 						</div>
 					</section>
 				</section>
@@ -90,10 +85,3 @@ export default function Projects({ starredRepos }) {
 		</main>
 	);
 }
-
-/* 
-possible
-c5fffd \ celeste
-D6FFF6 / ming green
-
-*/
